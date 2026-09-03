eaaa3052f08872a756500d5dcc188c1e36a8473d
nathan270109 <natiel228@gmail.com>
2026-09-03 16:48:32 -0300

    remove catalog

--- draft-sport/src/app/pages/feats/catalog/catalog.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-catalog',
-  styleUrl: './catalog.css',
-  templateUrl: './catalog.html',
-})
-export class Catalog {}
