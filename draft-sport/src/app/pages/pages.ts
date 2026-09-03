e012246ae8f32a5275bae14e18a9465145923b4b
nathan270109 <natiel228@gmail.com>
2026-09-03 16:27:31 -0300

    adiciona pasta

---
+++ draft-sport/src/app/pages/pages.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-pages',
+  styleUrl: './pages.css',
+  templateUrl: './pages.html',
+})
+export class Pages {}
