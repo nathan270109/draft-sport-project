933d310375de600ab6a307720a31b9968cdaf6bd
nathan270109 <natiel228@gmail.com>
2026-09-04 17:10:44 -0300

    cria carrossel
    feats/home/carrossel

---
+++ draft-sport/src/app/feats/home/carrossel/carrossel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-carrossel',
+  styleUrl: './carrossel.css',
+  templateUrl: './carrossel.html',
+})
+export class Carrossel {}
