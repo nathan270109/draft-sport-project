5cb5806303a252cb4bd4f7883b1c7b70f54b6952
nathan270109 <natiel228@gmail.com>
2026-09-04 23:30:03 -0300

    fix: arruma path
    app/app.routes

--- draft-sport/src/app/app.routes.ts
+++ draft-sport/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './feats/home/home';
+import { Cart } from './feats/cart/cart';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: Home},
+    { path: 'cart', component: Cart}
+
+];
