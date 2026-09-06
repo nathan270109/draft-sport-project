95c7b9b53de4c723e7b0ba13884106eef1f678c6
nathan270109 <natiel228@gmail.com>
2026-09-05 21:26:39 -0300

    feat: adiciona header no imports

--- draft-sport/src/app/feats/home/home.ts
+++ draft-sport/src/app/feats/home/home.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero/hero';
 import { Carrossel } from './carrossel/carrossel';
+import { Header } from '../../components/header/header';
 
 
 
 @Component({
-  imports: [Hero,Carrossel],
+  imports: [Hero,Carrossel, Header],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
