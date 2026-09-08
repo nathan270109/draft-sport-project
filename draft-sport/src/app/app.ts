7817f8b40dd7307dffa4dd7d050ba9e15a2b115a
nathan270109 <natiel228@gmail.com>
2026-09-08 15:53:53 -0300

    fix: arruma imports

--- draft-sport/src/app/app.ts
+++ draft-sport/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Feats } from "./feats/feats";
 import { Home } from "./feats/home/home";
 
 @Component({
-  imports: [RouterOutlet, Feats, Home],
+  imports: [RouterOutlet, Home],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
