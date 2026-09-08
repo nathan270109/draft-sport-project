0b4b302c1f98cc15a14bade2e8c7148b35bcec72
nathan270109 <natiel228@gmail.com>
2026-09-08 17:34:39 -0300

    Revert "feats/home"

--- draft-sport/src/app/app.ts
+++ draft-sport/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from "./feats/home/home";
+import { Header } from "./components/header/header";
 
 @Component({
-  imports: [RouterOutlet, Home],
+  imports: [RouterOutlet, Header],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
--- draft-sport/src/app/feats/feats.ts
+++ draft-sport/src/app/feats/feats.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { Cart } from "./cart/cart";
+import { Home } from "./home/home";
+import { Produtos } from "./produtos/produtos";
 
 @Component({
-  imports: [],
+  imports: [Cart, Home, Produtos],
   selector: 'app-feats',
   styleUrl: './feats.css',
   templateUrl: './feats.html',
