cea95577365deea86b809d478291a57cd74053e7
katheriny <katherinykc13@gmail.com>
2026-09-09 11:25:12 -0300

    fix: ajusta comando de início para especificar o host e adiciona nova linha no arquivo de rotas

--- draft-sport/src/app/app.routes.ts
+++ draft-sport/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Home } from './feats/home/home';
 import { Cart } from './feats/cart/cart';
 
+
 export const routes: Routes = [
 
     { path: '', redirectTo: 'home', pathMatch: 'full'},
