bcaf45a56789cfe5a8480f432f2b9b62336a52d3
Maria Letícia da Silva <marials2607.sl@gmail.com>
2026-09-09 18:09:13 -0300

    finaliza carrinho
    feats/cart

--- draft-sport/src/app/feats/cart/cart.ts
+++ draft-sport/src/app/feats/cart/cart.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { CartService } from './cart-service';
-import { ProdutoCarrinho } from './produto-carrinho';
+
 @Component({
   imports: [],
   selector: 'app-cart',
@@ -9,20 +9,6 @@ import { ProdutoCarrinho } from './produto-carrinho';
 })
 export class Cart {
   protected readonly cartService = inject(CartService);
-
-  adicionarProdutoTeste() {
-    const produto: ProdutoCarrinho = {
-      id: 1,
-      nome: 'Tênis Adistar Control 5',
-      descricao: 'Cloud White / Orbit Grey / Champagne Met.',
-      preco: 999.99,
-      imagem: '',
-      tamanho: '37',
-      quantidade: 1
-    };
-
-    this.cartService.adicionarProduto(produto);
-  }
 
   formatarPreco(valor: number) {
     return valor.toLocaleString('pt-BR', {
