ba4aab7a42f95b24dc77afff49effb06e277ad45
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-10 17:07:14 -0300

    cria componente marcas-parceiras dentro da pagina home

---
+++ src/app/feats/home/marcas-parceiras/marcas-parceiras.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-marcas-parceiras',
+  styleUrl: './marcas-parceiras.css',
+  templateUrl: './marcas-parceiras.html',
+})
+export class MarcasParceiras {}
