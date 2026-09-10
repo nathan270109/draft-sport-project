ffd68ae2bee777fa8b8530e4e539af533f9c3884
nathan270109 <natiel228@gmail.com>
2026-09-10 16:03:10 -0300

    fix: arruma imports

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { Carrossel } from "./carrossel/carrossel";
 import { Hero } from "./hero/hero";
-import { Footer } from "../../components/footer/footer";
 import { Informacoes } from "./informacoes/informacoes";
 
 @Component({
-  imports: [Carrossel, Hero, Footer, Informacoes],
+  imports: [Carrossel, Hero, Informacoes],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
