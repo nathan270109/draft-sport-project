fa2a7c01f90b1abcace76715ede9b66b15fec5e9
nathan270109 <natiel228@gmail.com>
2026-09-10 15:45:22 -0300

    feat: conecta informacoes

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Carrossel } from "./carrossel/carrossel";
 import { Hero } from "./hero/hero";
 import { Footer } from "../../components/footer/footer";
+import { Informacoes } from "./informacoes/informacoes";
 
 @Component({
-  imports: [Carrossel, Hero, Footer],
+  imports: [Carrossel, Hero, Footer, Informacoes],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
