c70fc0da907b0aca797d76eb8dc7ab17a033a0b5
nathan270109 <natiel228@gmail.com>
2026-09-10 15:43:34 -0300

    feat: adiciona caminho footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { Footer } from "./components/footer/footer";
 
 
 @Component({
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
