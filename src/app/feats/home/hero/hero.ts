59db726d2a48ecbe07f1e088d4b6aa6e1fde48e2
nathan270109 <natiel228@gmail.com>
2026-09-11 14:52:54 -0300

    style: arruma umas coisas

--- src/app/feats/home/hero/hero.ts
+++ src/app/feats/home/hero/hero.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-hero',
   styleUrl: './hero.css',
   templateUrl: './hero.html',
