20f2b4b556a1c5cb42cd33e639164c199f2360f8
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-10 23:38:00 -0300

    conecta seção marcas-parceiras na página home

--- src/app/feats/home/home.ts
+++ src/app/feats/home/home.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { Carrossel } from "./carrossel/carrossel";
 import { Hero } from "./hero/hero";
 import { Informacoes } from "./informacoes/informacoes";
+import { MarcasParceiras } from "./marcas-parceiras/marcas-parceiras";
 
 @Component({
-  imports: [Carrossel, Hero, Informacoes],
+  imports: [Carrossel, Hero, Informacoes, MarcasParceiras],
   selector: 'app-home',
   styleUrl: './home.css',
   templateUrl: './home.html',
