68bd5a3740a7a6203b93c6d55fbb2c16982f5532
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-13 18:44:21 -0300

    cria componente admin

---
+++ src/app/feats/produtos-api/admin/admin.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-admin',
+  styleUrl: './admin.css',
+  templateUrl: './admin.html',
+})
+export class Admin {}
