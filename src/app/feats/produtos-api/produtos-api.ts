eea276aeab77f564c024cf94ea8fd33acd56b889
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-13 18:43:10 -0300

    cria componente produtos-api

---
+++ src/app/feats/produtos-api/produtos-api.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-produtos-api',
+  styleUrl: './produtos-api.css',
+  templateUrl: './produtos-api.html',
+})
+export class ProdutosApi {}
