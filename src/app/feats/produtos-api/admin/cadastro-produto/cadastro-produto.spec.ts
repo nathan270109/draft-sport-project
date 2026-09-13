7b0fecf9f4a92d2dd0bed14f0a8c0763b8208be4
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-13 18:49:38 -0300

    cria componente cadastro-produto

---
+++ src/app/feats/produtos-api/admin/cadastro-produto/cadastro-produto.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CadastroProduto } from './cadastro-produto';
+
+describe('CadastroProduto', () => {
+  let component: CadastroProduto;
+  let fixture: ComponentFixture<CadastroProduto>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CadastroProduto],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CadastroProduto);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/feats/produtos-api/admin/cadastro-produto/cadastro-produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-cadastro-produto',
+  styleUrl: './cadastro-produto.css',
+  templateUrl: './cadastro-produto.html',
+})
+export class CadastroProduto {}
