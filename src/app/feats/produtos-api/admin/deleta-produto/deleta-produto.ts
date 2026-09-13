a63373c68b0db05f792200b1593db2356273d058
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-13 18:51:41 -0300

    cria componente deleta-produto

---
+++ src/app/feats/produtos-api/admin/deleta-produto/deleta-produto.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-deleta-produto',
+  styleUrl: './deleta-produto.css',
+  templateUrl: './deleta-produto.html',
+})
+export class DeletaProduto {}
