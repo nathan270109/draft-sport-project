4de3efbfe9fe8d8446724f09ee8e97c7e852bc7b
Eduardo Nascimento Alves <eduardonasm10@gmail.com>
2026-09-14 15:14:43 -0300

    feat: add botão de voltar para loja depois de finalizar compra

--- src/app/feats/compra-realizada/compra-realizada.ts
+++ src/app/feats/compra-realizada/compra-realizada.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
-  imports: [],
+  imports: [RouterLink],
   selector: 'app-compra-realizada',
   styleUrl: './compra-realizada.css',
   templateUrl: './compra-realizada.html',
