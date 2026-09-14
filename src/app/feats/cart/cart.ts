b286609540bfcdeb88bf0be20ca4254ba595960c
Maria Letícia da Silva <marials2607.sl@gmail.com>
2026-09-14 14:39:14 -0300

    correcao finalizar compra
    feats/cart

--- src/app/feats/cart/cart.ts
+++ src/app/feats/cart/cart.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { CartService } from './cart-service';
 
 @Component({
@@ -10,10 +11,17 @@ import { CartService } from './cart-service';
 export class Cart {
   protected readonly cartService = inject(CartService);
 
+  private readonly router = inject(Router);
+
   formatarPreco(valor: number) {
     return valor.toLocaleString('pt-BR', {
       style: 'currency',
       currency: 'BRL'
     });
+  }
+
+  finalizarCompra() {
+    this.cartService.esvaziarCarrinho();
+    this.router.navigate(['/compra-realizada']);
   }
 }
